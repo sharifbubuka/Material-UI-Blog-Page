11191afda3496096253fe27de52e4cb4659c23c4
Bubuka Sharif <sharifbubuka256@gmail.com>
2020-12-28 02:55:59 +0300

    blog built -version1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import{ AppBar, Toolbar, Typography, Box, Container, Grid } from '@material-ui/core'
+import { Pagination } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles'
+import Article from './Article'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: '#fff'
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+     url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: '500px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: '4rem',
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: '3em'
+    }
+    },
+    blogsContainer: {
+      paddingTop: theme.spacing(3),
+    },
+    blogTitle: {
+      fontWeight: 800,
+      paddingBottom: theme.spacing(1)
+    },
+    paginationContainer: {
+      display: 'flex',
+      justifyContent: 'center'
+    }
+}))
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="primary">
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
+
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle}>
+          Articles
+        </Typography>
+        <Grid container spacing={3}>
+          <Article image="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="man smiling" title="React UseContext" />
+          <Article image="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="woman smiling" title="Testing with Jest" />
+          <Article image="https://images.pexels.com/photos/3785079/pexels-photo-3785079.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="happy man" title="Github Profile File" />
+          {/* <Article image="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="man smiling" title="React UseContext" /> */}
+        </Grid>
+        <Box my={4} className={classes.paginationContainer}>
+          <Pagination count={10} />
+        </Box>
+      </Container>
     </div>
   );
 }
---
+++ src/Article.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Grid, Card, CardActionArea, CardContent, CardActions, CardMedia, Typography, Box, Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: '100%'
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: 'flex',
+    margin: '0 10px',
+    justifyContent: 'space-between'
+  },
+  author: {
+    display: 'flex'
+  }
+}))
+
+function Article({image, alt, title}) {
+  const classes = useStyles()
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            component="img"
+            alt={alt}
+            height="140"
+            image={image}
+            title={alt}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+            <Avatar src={image} />
+            <Box ml={2}>
+              <Typography variant="subtitle2" component="p">Bubuka Sharif</Typography>
+              <Typography variant="subtitle2" color="textSecondary" component="p">May 14, 2020</Typography>
+            </Box>
+          </Box>
+          <Box>
+            <BookmarkBorderIcon />
+          </Box>
+        </CardActions>
+      </Card>
+    </Grid>
+  )
+}
+
+export default Article
